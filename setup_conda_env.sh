d32dc9482aec27cdbf4beb3fec608563a91a223c
Sreenivas Manyam Rajaram <smanyamrajar@vmware.com>
2023-12-08 10:48:36 -0800

    experimenting with requirements.txt

--- setup_conda_env.sh
+++ setup_conda_env.sh
@@ -1,5 +1,10 @@
+# Set up conda environment for wishwell
 conda create -n wishwell python=3.9
 conda activate wishwell
 python3 --version
 python3 -m pip install --upgrade pip
 pip install -r requirements.txt
+
+# Cleanup conda environment for wishwell
+conda deactivate
+conda env remove --name wishwell    
